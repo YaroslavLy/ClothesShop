e893aa3e12440f2cc3417893c916f946dcbbcbc7
Yaroslav Lysanets <yaroslav.lysanets@shc.digital>
2022-11-13 17:07:48 +0100

    add glide and cart

--- app/src/main/java/com/example/clothesshop/ui/product/ProductRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/clothesshop/ui/product/ProductRecyclerViewAdapter.kt
@@ -1,9 +1,6 @@
 package com.example.clothesshop.ui.product
 
-import android.annotation.SuppressLint
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.os.AsyncTask
+
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -12,19 +9,22 @@ import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.clothesshop.GlideApp
+
 import com.example.clothesshop.R
 import com.example.clothesshop.model.Product
+
 
 class ProductRecyclerViewAdapter(private val mList: List<Product>) :
     RecyclerView.Adapter<ProductRecyclerViewAdapter.ViewHolder>() {
 
     //val context: Context
-
+    lateinit var view: View
     // create new views
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         // inflates the card_view_design view
         // that is used to hold list item
-        val view = LayoutInflater.from(parent.context)
+         view = LayoutInflater.from(parent.context)
             .inflate(R.layout.fragment_product, parent, false)
 
         return ViewHolder(view)
@@ -34,47 +34,26 @@ class ProductRecyclerViewAdapter(private val mList: List<Product>) :
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
         val ItemsViewModel = mList[position]
-
-        // sets the image to the imageview from our itemHolder class
-        //ItemsViewModel.image
-        //holder.imageView.setImageResource(ItemsViewModel.image)
-
-        // sets the text to the textview from our itemHolder class
-        //holder.textView.text = ItemsViewModel.text
+        
 
         holder.textName.text=ItemsViewModel.name
         //holder.textCode.text=ItemsViewModel.code
         holder.textPrice.text=ItemsViewModel.price
-        DownloadImageFromInternet(holder.imageView).execute(ItemsViewModel.image)
+
+        GlideApp.with(holder.imageView.context)
+            .load(ItemsViewModel.image)
+            .error(R.drawable.ic_baseline_autorenew_24)
+            .into(holder.imageView)
         holder.linearLayout.setOnClickListener{
             Log.i("tag","Hice")
         }
+        holder.buttonBasket.setOnClickListener {
+            holder.buttonBasket.setImageResource(R.drawable.ic_baseline_shopping_cart_24_2)
+        }
 
     }
 
-    @SuppressLint("StaticFieldLeak")
-    @Suppress("DEPRECATION")
-    private inner class DownloadImageFromInternet(var imageView: ImageView) : AsyncTask<String, Void, Bitmap?>() {
-        init {
-            //Toast.makeText(context, "Please wait, it may take a few minute...",     Toast.LENGTH_SHORT).show()
-        }
-        override fun doInBackground(vararg urls: String): Bitmap? {
-            val imageURL = urls[0]
-            var image: Bitmap? = null
-            try {
-                val `in` = java.net.URL(imageURL).openStream()
-                image = BitmapFactory.decodeStream(`in`)
-            }
-            catch (e: Exception) {
-                Log.e("Error Message", e.message.toString())
-                e.printStackTrace()
-            }
-            return image
-        }
-        override fun onPostExecute(result: Bitmap?) {
-            imageView.setImageBitmap(result)
-        }
-    }
+
 
     // return the number of the items in the list
     override fun getItemCount(): Int {
@@ -90,7 +69,7 @@ class ProductRecyclerViewAdapter(private val mList: List<Product>) :
        // val textCode: TextView = itemView.findViewById(R.id.textCode)
         val textPrice: TextView = itemView.findViewById(R.id.textPrice)
         val linearLayout: LinearLayout = itemView.findViewById(R.id.linearLayout)
-
+        val buttonBasket: ImageView = itemView.findViewById(R.id.button_basket)
 
     }
 }
